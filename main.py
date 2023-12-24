1838ec0d0b7876544aadecc57a18ad2a3185381c
VoroninMaxim <maksimsazonov476@gmail.com>
2023-12-24 20:56:18 +0300

    run_tests.yml

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 from transformers import pipeline
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     text: str
+
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text)[0]
 
 @app.post("/predict/")
 def predict(item: Item):
