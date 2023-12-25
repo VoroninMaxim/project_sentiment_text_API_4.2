6be70f73cf528922e8b3ce568d07042b10eaa506
VoroninMaxim <maksimsazonov476@gmail.com>
2023-12-25 14:14:27 +0300

    tests-1

--- test_main.py
+++ test_main.py
@@ -3,12 +3,10 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-
 
 def test_predict_positive():
     response = client.post("/predict/",
@@ -16,7 +14,6 @@ def test_predict_positive():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
-
 
 def test_predict_negative():
     response = client.post("/predict/",
