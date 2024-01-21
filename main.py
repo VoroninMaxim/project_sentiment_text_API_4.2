5b3e74f4e3bbc60b118c5fd1b863679b00a5e44e
VoroninMaxim <maksimsazonov476@gmail.com>
2024-01-21 13:58:57 +0300

    tests-1

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-﻿from fastapi import FastAPI
-from transformers import pipeline
-from pydantic import BaseModel
+﻿import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import streamlit as st
 
+st.title('My Text Toxicity :blue[App] :sunglasses:')
 
-class Item(BaseModel):
-    text: str
+model_checkpoint = 'cointegrated/rubert-tiny-toxicity'
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
+if torch.cuda.is_available():
+    model.cuda()
 
+def text2toxicity(text, aggregate=True):
+    """ Calculate toxicity of a text (if aggregate=True) or a vector of toxicity aspects (if aggregate=False)"""
+    with torch.no_grad():
+        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(model.device)
+        proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()
+    if isinstance(text, str):
+        proba = proba[0]
+    if aggregate:
+        return 1 - proba.T[0] * (1 - proba.T[-1])
+    return proba
 
-app = FastAPI()
-classifier = pipeline("sentiment-analysis")
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
-
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+user_input = st.text_input("Enter a text")#-, key='enter_text', type='text'
+st.write("{:.6f}".format(text2toxicity(user_input)), "--non-toxic--")
